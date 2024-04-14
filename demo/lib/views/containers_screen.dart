b130b0fbe13b9d8f593170d65af25592161cca17
werner-scholtz <werner.scholtz@kdab.com>
2024-04-14 12:38:29 +0200

    Updated container screen.

--- demo/lib/views/containers_screen.dart
+++ demo/lib/views/containers_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+
 import 'package:tswiri/providers.dart';
 import 'package:tswiri/routes.dart';
 import 'package:tswiri/views/abstract_screen.dart';
@@ -12,14 +15,26 @@ class ContainersScreen extends ConsumerStatefulWidget {
   ConsumerState<ContainersScreen> createState() => _ContainersScreenState();
 }
 
-class _ContainersScreenState extends AbstractScreen<ContainersScreen> {
-  bool _isSearching = false;
-  String _searchQuery = '';
+enum ContainerScreenState {
+  normal,
+  searching,
+  editing,
+}
 
+class _ContainersScreenState extends AbstractScreen<ContainersScreen> {
   /// Returns a list of all [ContainerRelationship]s.
   Future<List<CatalogedContainer>> catalogedContainers() async {
     return db.catalogedContainers.where().findAll();
   }
+
+  ContainerScreenState state = ContainerScreenState.normal;
+  bool get isNormal => state == ContainerScreenState.normal;
+  bool get isEditing => state == ContainerScreenState.editing;
+  bool get isSearching => state == ContainerScreenState.searching;
+
+  final searchFocusNode = FocusNode();
+
+  //TODO: implement custom scroll view with search bar
 
   @override
   Widget build(BuildContext context) {
@@ -51,51 +66,9 @@ class _ContainersScreenState extends AbstractScreen<ContainersScreen> {
                 itemCount: items.length,
                 itemBuilder: (context, index) {
                   final item = items[index];
-                  final containerType = space.getContainerType(item.typeUUID);
-                  final name = Text(item.name.toString());
-                  final description =
-                      item.description != null && item.description!.isNotEmpty
-                          ? Text(item.description.toString())
-                          : null;
-                  final leading = Tooltip(
-                    message: containerType?.name,
-                    child: Icon(
-                      containerType?.iconData.iconData,
-                      color: containerType?.color.color,
-                    ),
-                  );
-
-                  return Card(
-                    clipBehavior: Clip.antiAlias,
-                    child: InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(
-                          context,
-                          Routes.container,
-                          arguments: item,
-                        );
-                      },
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          ListTile(
-                            leading: leading,
-                            title: name,
-                            subtitle: description,
-                          ),
-                          ListTile(
-                            leading: const Icon(Icons.qr_code),
-                            title: Text(
-                              item.barcodeUUID.toString(),
-                              maxLines: 1,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ),
+                  return CatalogedContainerWidget(
+                    container: item,
+                    onTap: () => _onTileTap(item),
                   );
                 },
               );
@@ -105,12 +78,47 @@ class _ContainersScreenState extends AbstractScreen<ContainersScreen> {
       },
     );
 
-    final appbar = AppBar(
-      title: TextField(),
+    final searchAction = IconButton(
+      onPressed: _startSearch,
+      icon: const Icon(Icons.search),
+    );
+
+    final scanAction = IconButton(
+      tooltip: 'Scan Barcode',
+      onPressed: _scanBarcode,
+      icon: const Icon(Icons.scanner),
+    );
+
+    final editButton = IconButton(
+      onPressed: _startEdit,
+      icon: const Icon(Icons.edit),
+    );
+
+    final resetButton = IconButton(
+      tooltip: 'Cancel',
+      onPressed: _resetState,
+      icon: const Icon(Icons.close),
+    );
+
+    final searchBar = TextFormField(
+      focusNode: searchFocusNode,
+      decoration: const InputDecoration(
+        contentPadding: EdgeInsets.symmetric(vertical: 2, horizontal: 8),
+      ),
+    );
+
+    final appBar = AppBar(
+      leading: isNormal ? editButton : null,
+      title: isSearching ? searchBar : null,
+      actions: [
+        if (isNormal) scanAction,
+        if (isNormal) searchAction,
+        if (!isNormal) resetButton,
+      ],
     );
 
     return Scaffold(
-      appBar: appbar,
+      appBar: appBar,
       body: body,
       floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       floatingActionButton: FloatingActionButton.extended(
@@ -119,6 +127,112 @@ class _ContainersScreenState extends AbstractScreen<ContainersScreen> {
         },
         icon: const Icon(Icons.add),
         label: const Text('Add Container'),
+      ),
+    );
+  }
+
+  void _scanBarcode() async {
+    final barcodeUUID = await scanBarcode();
+    if (barcodeUUID == null) return;
+
+    final container = await space.getCatalogedContainer(
+      barcodeUUID: barcodeUUID,
+    );
+
+    if (container == null) {
+      showSnackbar('No container linked');
+      return;
+    }
+
+    // ignore: use_build_context_synchronously
+    Navigator.of(context).pushNamed(Routes.container, arguments: container);
+  }
+
+  void _startEdit() {
+    setState(() {
+      state = ContainerScreenState.editing;
+    });
+  }
+
+  void _startSearch() {
+    setState(() {
+      state = ContainerScreenState.searching;
+      searchFocusNode.requestFocus();
+    });
+  }
+
+  void _resetState() {
+    setState(() {
+      state = ContainerScreenState.normal;
+      searchFocusNode.unfocus();
+    });
+  }
+
+  void _onTileTap(CatalogedContainer container) {
+    if (isEditing) {
+      /// TODO: add to editing list.
+    } else {
+      Navigator.pushNamed(
+        context,
+        Routes.container,
+        arguments: container,
+      );
+    }
+  }
+}
+
+class CatalogedContainerWidget extends ConsumerWidget {
+  final CatalogedContainer container;
+  final void Function() onTap;
+
+  const CatalogedContainerWidget({
+    super.key,
+    required this.container,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final space = ref.read(spaceProvider);
+    final containerType = space.getContainerType(container.typeUUID);
+    final name = Text(container.name.toString());
+    final description =
+        container.description != null && container.description!.isNotEmpty
+            ? Text(container.description.toString())
+            : null;
+
+    final leading = Tooltip(
+      message: containerType?.name,
+      child: Icon(
+        containerType?.iconData.iconData,
+        color: containerType?.color.color,
+      ),
+    );
+
+    return Card(
+      clipBehavior: Clip.antiAlias,
+      child: InkWell(
+        onTap: onTap,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            ListTile(
+              leading: leading,
+              title: name,
+              subtitle: description,
+            ),
+            ListTile(
+              leading: const Icon(Icons.qr_code),
+              title: Text(
+                container.barcodeUUID.toString(),
+                maxLines: 1,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
